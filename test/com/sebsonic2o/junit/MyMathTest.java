92ca4f092c105935591c38f5be110bc8ee07352a
Sebastien Cheraux <scheraux@gmail.com>
2020-12-18 09:19:34 -0800

    add junit tests with code assertion

--- test/com/sebsonic2o/junit/MyMathTest.java
+++ test/com/sebsonic2o/junit/MyMathTest.java
@@ -1,11 +1,20 @@
 package com.sebsonic2o.junit;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class MyMathTest {
 
+	MyMath myMath = new MyMath();
+
 	@Test
-	public void test() {
-		// absence of failure is success
+	public void sumOf3() {
+		assertEquals(6, myMath.sum(new int[] {1, 2, 3}));
+	}
+
+	@Test
+	public void sumOf1() {
+		assertEquals(3, myMath.sum(new int[] {3}));
 	}
 }

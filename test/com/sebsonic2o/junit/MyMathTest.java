c298a59ace6c6676f288d9e6318cffe91e2f8858
Sebastien Cheraux <scheraux@gmail.com>
2020-12-18 09:45:34 -0800

    leverage junit before/after annotations

--- test/com/sebsonic2o/junit/MyMathTest.java
+++ test/com/sebsonic2o/junit/MyMathTest.java
@@ -2,19 +2,45 @@ package com.sebsonic2o.junit;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MyMathTest {
 
 	MyMath myMath = new MyMath();
 
+	@Before
+	public void doBefore() {
+		System.out.println("action before each test");
+	}
+
+	@After
+	public void doAfter() {
+		System.out.println("action after each test");
+	}
+
+	@BeforeClass
+	public static void doBeforeAll() {
+		System.out.println("action before all test");
+	}
+
+	@AfterClass
+	public static void doAfterAll() {
+		System.out.println("action after all test");
+	}
+
 	@Test
 	public void sumOf3() {
+		System.out.println("test 1");
 		assertEquals(6, myMath.sum(new int[] {1, 2, 3}));
 	}
 
 	@Test
 	public void sumOf1() {
+		System.out.println("test 2");
 		assertEquals(3, myMath.sum(new int[] {3}));
 	}
 }
